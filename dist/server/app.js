66e93f84dc3be8922966f72f9abe5a8d0027b728
Kamran Tariq <kamrankami55@outlook.com>
2018-01-17 20:11:55 +0500

    deploying to heroku

---
+++ dist/server/app.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var bodyParser = require("body-parser");
+var dotenv = require("dotenv");
+var express = require("express");
+var mongoose = require("mongoose");
+var path = require("path");
+var routes_1 = require("./routes");
+var app = express();
+exports.app = app;
+dotenv.load({ path: '.env' });
+app.set('port', (process.env.PORT || 3000));
+app.use('/', express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+var mongodbURI = 'mongodb://admin:valleyforge@ds247587.mlab.com:47587/trainer-app';
+// let mongodbURI;
+// if (process.env.NODE_ENV === 'test') {
+//   mongodbURI = process.env.MONGODB_TEST_URI;
+// } else {
+//   mongodbURI = process.env.MONGODB_URI;
+//   app.use(morgan('dev'));
+// }
+mongoose.Promise = global.Promise;
+var mongodb = mongoose.connect(mongodbURI, { useMongoClient: true });
+mongodb
+    .then(function (db) {
+    console.log('Connected to MongoDB on', db.host + ':' + db.port);
+    routes_1.default(app);
+    app.get('/*', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+    if (!module.parent) {
+        app.listen(app.get('port'), function () {
+            console.log('Angular Full Stack listening on port ' + app.get('port'));
+        });
+    }
+})
+    .catch(function (err) {
+    console.error(err);
+});
+//# sourceMappingURL=app.js.map
---
+++ dist/server/models/course.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mongoose = require("mongoose");
+var courseSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    price: Number,
+    eta: String,
+    description: String,
+    isPublished: Boolean,
+    userCount: Number,
+    syllabus: String,
+    content: {
+        chapters: [{
+                title: String,
+                lessons: [{
+                        title: String,
+                        content: String,
+                        image: String
+                    }]
+            }]
+    }
+});
+var course = mongoose.model('course', courseSchema);
+exports.default = course;
+//# sourceMappingURL=course.js.map
